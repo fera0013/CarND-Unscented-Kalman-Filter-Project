8bdd1c95fa0db733a5ca2844da023f71a53bb3df
fera0013 <ralphfehrer@gmail.com>
2017-06-05 20:27:59 +0200

    Initial submit Project builds in VS2017

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include <uWS/uWS.h>
+#include "uWS\uWS.h"
 #include <iostream>
 #include "json.hpp"
 #include <math.h>
@@ -13,17 +13,19 @@ using json = nlohmann::json;
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-std::string hasData(std::string s) {
-  auto found_null = s.find("null");
-  auto b1 = s.find_first_of("[");
-  auto b2 = s.find_first_of("]");
-  if (found_null != std::string::npos) {
-    return "";
-  }
-  else if (b1 != std::string::npos && b2 != std::string::npos) {
-    return s.substr(b1, b2 - b1 + 1);
-  }
-  return "";
+std::stringstream hasData(std::string s) {
+	auto found_null = s.find("null");
+	auto b1 = s.find_first_of("[");
+	auto b2 = s.find_last_of("]");
+	if (found_null != std::string::npos) {
+		return std::stringstream();
+	}
+	else if (b1 != std::string::npos && b2 != std::string::npos) {
+		std::stringstream tmp = std::stringstream();
+		tmp.str(s.substr(b1, b2 - b1 + 1));
+		return tmp;
+	}
+	return std::stringstream();
 }
 
 int main()
@@ -47,7 +49,7 @@ int main()
     {
 
       auto s = hasData(std::string(data));
-      if (s != "") {
+      if (s.str() != "") {
       	
         auto j = json::parse(s);
 
